2b6243d399b792c3fc5311aeb1812f6e69424242
josh_chen <jack100398@gmail.com>
2023-03-06 22:35:59 +0800

    feat:前端footer修正

--- resources/views/Frontstage/zh/component/footer.blade.php
+++ resources/views/Frontstage/zh/component/footer.blade.php
@@ -3,11 +3,11 @@
         <a href="" class="footer_logo"><img src="{{ asset('Frontstage/images/footer_logo.png') }}"
                 alt=""></a>
         <ul>
-            <li><a href="">關於欣茂達</a></li>
-            <li><a href="news.html">最新消息</a></li>
-            <li><a href="product.html">產品介紹</a></li>
-            <li><a href="recommend.html">篩選推薦</a></li>
-            <li><a href="contact.html">聯絡我們</a></li>
+            <li><a href="{{ route('index') }}">關於欣茂達</a></li>
+            <li><a href="{{ route('news') }}">最新消息</a></li>
+            <li><a href="{{ route('product') }}">產品介紹</a></li>
+            <li><a href="{{ route('recommend') }}">篩選推薦</a></li>
+            <li><a href="{{ route('contact') }}">聯絡我們</a></li>
         </ul>
         <div class="links">
             <span class="company">欣茂達科技股份有限公司</span>
