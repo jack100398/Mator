45e32e2a997091744d1341d9c6a348f47176ce4f
josh_chen <jack100398@gmail.com>
2023-04-10 22:22:10 +0800

    篩選條件修正

--- app/Http/Repositories/CommodityRepository.php
+++ app/Http/Repositories/CommodityRepository.php
@@ -11,6 +11,13 @@ class CommodityRepository
         return Commodity::query()
             ->where('linear_ruler', $conditions['linear_ruler'])
             ->where('resolution', $conditions['resolution'])
+            ->where('travel', '>=', $conditions['distance'])
+            ->when($conditions['direction'] == 0, function ($query) use ($conditions) {
+                $query->where('vertical_load', '>=', $conditions['weight']);
+            })
+            ->when($conditions['direction'] == 1, function ($query) use ($conditions) {
+                $query->where('horizontal_load', '>=', $conditions['weight']);
+            })
             ->get();
     }
 }
