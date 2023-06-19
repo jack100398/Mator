eb9d3bdc4bf0583c62425e4690c67b6be41f6aaa
josh_chen <jack100398@gmail.com>
2023-06-19 18:09:41 +0800

    部署到 fly.io 測試環境

--- resources/views/Backstage/index.blade.php
+++ resources/views/Backstage/index.blade.php
@@ -1,9 +1,5 @@
 @extends('Backstage.layouts')
 @section('content')
-	<div id="app">
-		<!-- Page Heading -->
-        <h1 class="h3 mb-1 text-gray-800">欣茂達科技 後台管理系統</h1>
-        <p class="mb-4">歡迎登入 欣茂達科技 後台管理系統！請直接點選進入欲調整之單元。<br>
-        使用上若有任何問題，歡迎聯繫：02-2977-6769 鼎豐整合行銷-李先生</p>
-	</div>
+    <div id="app">
+    </div>
 @endsection
